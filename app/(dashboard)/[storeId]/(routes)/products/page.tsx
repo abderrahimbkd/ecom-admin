1c34f2d22c19ac5ca222b48e043ae579714bf84a
abderrahimbkd <80970245+abderrahimbkd@users.noreply.github.com>
2024-11-25 19:38:18 +0100

    sql to mongo

--- app/(dashboard)/[storeId]/(routes)/products/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -6,11 +6,7 @@ import { ProductsClient } from './components/productsClient';
 import { ProductColumn } from './components/columns';
 import { formatter } from '@/lib/utils';
 
-const ProductsPage = async ({
-  params,
-}: {
-  params: { storeId: string };
-}) => {
+const ProductsPage = async ({ params }: { params: { storeId: string } }) => {
   const products = await prismadb.product.findMany({
     where: {
       storeId: params.storeId,
@@ -25,19 +21,17 @@ const ProductsPage = async ({
     },
   });
   const { format } = require('date-fns');
-  const formattedProducts: ProductColumn[] = products.map(
-    (item) => ({
-      id: item.id,
-      name: item.name,
-      isFeatured: item.isFeatured,
-      isArchived: item.isArchived,
-      price: formatter.format(item.price.toNumber()),
-      category: item.category.name,
-      size: item.size.name,
-      color: item.color.value,
-      createdAt: format(item.createdAt, 'MMMM do, yyyy'),
-    })
-  );
+  const formattedProducts: ProductColumn[] = products.map((item) => ({
+    id: item.id,
+    name: item.name,
+    isFeatured: item.isFeatured,
+    isArchived: item.isArchived,
+    price: formatter.format(item.price),
+    category: item.category.name,
+    size: item.size.name,
+    color: item.color.value,
+    createdAt: format(item.createdAt, 'MMMM do, yyyy'),
+  }));
 
   return (
     <div className="flex-col">
